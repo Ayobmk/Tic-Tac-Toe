a708279019ca59b277886ce18768a1a40875c423
ayoub <Elma0076@algonquinlive.com>
2024-05-31 01:56:47 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css'
+
+const Square = () => {
+  return (
+    <div className="square">
+      X
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20,
+    }}>
+      Board
+      <div className="board-row">
+       <Square /> <Square /> <Square />
+      </div>
+      <div className="board-row">
+       <Square /> <Square /> <Square />
+      </div>
+      <div className="board-row">
+       <Square /> <Square /> <Square />
+      </div>
+    </div>
+  );
+};  
+
+const Game = () => {
+  return (
+    <div className='game'>
+      Game
+      <Board />
+    </div>
+  );
+};
+
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
